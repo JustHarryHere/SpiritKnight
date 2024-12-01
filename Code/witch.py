1fea32076a2155d18b869822d135661b44467bce
VuongVuiVe123 <dangvuongv3@gmail.com>
2024-12-01 20:33:45 +0700

    Update witch.py

--- Code/witch.py
+++ Code/witch.py
@@ -4,11 +4,11 @@ from PIL import Image
 pygame.init()
 
 width = 1280
-height= 720
+height = 720
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
-#Char
+# Character
 character_gif_path = 'D:/SpiritKnight/Sprites/lil dude bigger.gif'
 character_gif = Image.open(character_gif_path)
 char_frames = []
@@ -17,16 +17,33 @@ try:
         char_frame = character_gif.copy()
         char_frame = char_frame.convert("RGBA")
         char_frames.append(pygame.image.fromstring(char_frame.tobytes(), char_frame.size, char_frame.mode))
-        character_gif.seek(len(char_frames))  # Move to the next frame
+        character_gif.seek(len(char_frames))
 except EOFError:
     pass
 
 flipped_frames = [pygame.transform.flip(char_frame, True, False) for char_frame in char_frames]
 char_rect = char_frames[0].get_rect(center=(width//2, height//2))
+
+# Enemy
+enemy_gif_path = 'D:\SpiritKnight\Sprites/black Wizard.gif'
+enemy_gif = Image.open(enemy_gif_path)
+enemy_frames = []
+try:
+    while True:
+        enemy_frame = enemy_gif.copy()
+        enemy_frame = enemy_frame.convert("RGBA")
+        enemy_frames.append(pygame.image.fromstring(enemy_frame.tobytes(), enemy_frame.size, enemy_frame.mode))
+        enemy_gif.seek(len(enemy_frames))
+except EOFError:
+    pass
+
+enemy_rect = enemy_frames[0].get_rect(center=(width//2 + 200, height//2))
+
 frame_index = 0
-cross_frame_index = 0
 frame_counter = 0
 frame_update_rate = 5
+enemy_frame_index = 0
+enemy_frame_counter = 0
 flipped = False
 
 while True:
@@ -47,18 +64,26 @@ while True:
     if keys[pygame.K_s]:
         char_rect.y += 5
 
-    # Cập nhật khung hình
+    # Update character frame index
     frame_counter += 1
     if frame_counter >= frame_update_rate:
         frame_counter = 0
         frame_index = (frame_index + 1) % len(char_frames)
 
-    # Hiển thị nhân vật
+    # Update enemy frame index
+    enemy_frame_counter += 1
+    if enemy_frame_counter >= frame_update_rate:
+        enemy_frame_counter = 0
+        enemy_frame_index = (enemy_frame_index + 1) % len(enemy_frames)
+
+    # Display character
     if flipped:
         screen.blit(flipped_frames[frame_index], char_rect)
     else:
         screen.blit(char_frames[frame_index], char_rect)
 
+    # Display enemy
+    screen.blit(enemy_frames[enemy_frame_index], enemy_rect)
+
     pygame.display.flip()
     clock.tick(60)
-    
