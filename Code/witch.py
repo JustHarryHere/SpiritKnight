6f00e453a4666bc4c10f2aa013fb76dcb2ed4e41
VuongVuiVe123 <dangvuongv3@gmail.com>
2024-12-01 20:30:00 +0700

    Create witch.py

---
+++ Code/witch.py
@@ -0,0 +1,64 @@
+import pygame, sys
+from PIL import Image
+
+pygame.init()
+
+width = 1280
+height= 720
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+
+#Char
+character_gif_path = 'D:/SpiritKnight/Sprites/lil dude bigger.gif'
+character_gif = Image.open(character_gif_path)
+char_frames = []
+try:
+    while True:
+        char_frame = character_gif.copy()
+        char_frame = char_frame.convert("RGBA")
+        char_frames.append(pygame.image.fromstring(char_frame.tobytes(), char_frame.size, char_frame.mode))
+        character_gif.seek(len(char_frames))  # Move to the next frame
+except EOFError:
+    pass
+
+flipped_frames = [pygame.transform.flip(char_frame, True, False) for char_frame in char_frames]
+char_rect = char_frames[0].get_rect(center=(width//2, height//2))
+frame_index = 0
+cross_frame_index = 0
+frame_counter = 0
+frame_update_rate = 5
+flipped = False
+
+while True:
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        char_rect.x -= 5
+        flipped = False
+    if keys[pygame.K_d]:
+        char_rect.x += 5
+        flipped = True
+    if keys[pygame.K_w]:
+        char_rect.y -= 5
+    if keys[pygame.K_s]:
+        char_rect.y += 5
+
+    # Cập nhật khung hình
+    frame_counter += 1
+    if frame_counter >= frame_update_rate:
+        frame_counter = 0
+        frame_index = (frame_index + 1) % len(char_frames)
+
+    # Hiển thị nhân vật
+    if flipped:
+        screen.blit(flipped_frames[frame_index], char_rect)
+    else:
+        screen.blit(char_frames[frame_index], char_rect)
+
+    pygame.display.flip()
+    clock.tick(60)
+    
